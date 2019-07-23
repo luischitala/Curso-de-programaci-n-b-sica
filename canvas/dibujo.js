74baf078caa95aec1436a2878c29b43e1ac3f2b1
Luis Chitala <itastetaco@gmail.com>
2019-07-23 02:40:13 -0500

    Primera funcion declarada

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,9 +1,13 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100, 100);
-lienzo.lineTo(200, 200);
-lienzo.stroke();
-lienzo.closePath();
+dibujarLinea("pink",10,300,220,10);
+dibujarLinea("yellow",300,10,10,220);
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.moveTo(xinicial, xfinal);
+	lienzo.lineTo(yinicial, yfinal);
+	lienzo.stroke();
+	lienzo.closePath();
+}
