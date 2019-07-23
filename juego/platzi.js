346adabd582d17a41ab9c96fd11e2d966cf9592c
Luis Chitala <itastetaco@gmail.com>
2019-07-23 18:47:19 -0500

    Correcion y cambio de variables a objetos

--- juego/platzi.js
+++ juego/platzi.js
@@ -2,14 +2,50 @@ var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
 var mapa = "tile.png";
 
-var imagen = new Image();
-imagen.src = mapa;
-imagen.addEventListener("load",dibujar);
+var fondo = {
+	url: "tile.png",
+	cargaOk:false
+}
+
+var vaca = {
+	url:"vaca.png",
+	cargaOk: false
+}
+
+fondo.objeto = new Image();
+fondo.objeto.src = fondo.url;
+fondo.objeto.addEventListener("load",cargarFondo);
+
+vaca.objeto = new Image();
+vaca.objeto.src = vaca.url;
+vaca.objeto.addEventListener("load", cargarVacas);
+
+function cargarFondo()
+{
+	fondo.cargaOK = true;
+	dibujar();
+}
+
+function cargarVacas()
+{
+	vaca.cargaOK = true;
+	dibujar();
+}
 
 function dibujar()
 {
-	papel.drawImage(imagen, 0,0);
+	
+	if(fondo.cargaOK)
+	{
+		papel.drawImage(fondo.objeto, 0,0);
+	}
+	if(vaca.cargaOK)
+	{
+		papel.drawImage(vaca.objeto, 0,0);
+	}
 }
+
+
 
 function aleatorio(min, maxi)
 {
