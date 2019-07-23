0006bfd80b1439625941f17a914be40167bc7b6b
Luis Chitala <itastetaco@gmail.com>
2019-07-23 14:39:24 -0500

    Creacion de funcion con bloque de texto

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -1,9 +1,14 @@
+var teclas = 
+{
+	UP: 38,
+	DOWN: 40,
+	LEFT: 37,
+	RIGHT: 39
+};
+console.log(teclas);
 document.addEventListener("keyup", dibujarTeclado);
-
-
 
 function dibujarTeclado(evento)
 {
-
 	console.log(evento.keyCode);
 }
