ad0a2c35acb4ca9f2be6e87110aa02e492da283f
Luis Chitala <itastetaco@gmail.com>
2019-07-23 15:27:10 -0500

    Correcion final del programa de dibujo

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -1,33 +1,50 @@
-var teclas = 
-{
-	UP: 38,
-	DOWN: 40,
-	LEFT: 37,
-	RIGHT: 39
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
 };
-console.log(teclas);
-document.addEventListener("keyup", dibujarTeclado);
+
+document.addEventListener("keydown", dibujarTeclado);
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+var x = 150;
+var y = 150;
+
+dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
 
 function dibujarTeclado(evento)
 {
-	
-	switch(evento.keyCode)
-	{
-		case teclas.UP:
-			console.log("ARRIBA");
-		break;
-
-		case teclas.DOWN:
-			console.log("ABAJO");
-		break;
-		
-		case teclas.LEFT:
-			console.log("IZQUIERDA");
-		break;
-
-		case teclas.RIGHT:
-			console.log("DERECHA");
-		break;
-	}
-
+  var colorcito = "#FAA";
+  var movimiento = 5;
+  switch(evento.keyCode)
+  {
+    case teclas.UP:
+      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+      y = y - movimiento;
+    break;
+    case teclas.DOWN:
+      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+      y = y + movimiento;
+    break;
+    case teclas.LEFT:
+      dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+      x = x - movimiento;
+    break;
+    case teclas.RIGHT:
+      dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+      x = x + movimiento;
+    break;
+  }
 }
