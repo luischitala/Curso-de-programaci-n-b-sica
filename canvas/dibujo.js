4a67e0b7fc21649f395492f6789d151396489bf9
Luis Chitala <itastetaco@gmail.com>
2019-07-23 03:37:54 -0500

    Dibujo con lineas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,7 +1,20 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-dibujarLinea("pink",10,300,220,10);
-dibujarLinea("yellow",300,10,10,220);
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorcito = "#FAA";
+while(l < lineas)
+{
+	yi = 10 * l;
+	xf = 10 * (l+1);
+	dibujarLinea(colorcito,0,yi,xf,300);
+	console.log("Linea" + 1);
+	l = l + 1;
+}
+
+dibujarLinea(colorcito, 1,1,1,300);
+dibujarLinea(colorcito, 1,299,299,299);
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
 	lienzo.beginPath();
