b3cbb816d4599d2e9c6692de9c71440ca70ad650
Luis Chitala <itastetaco@gmail.com>
2019-07-23 14:01:51 -0500

    Correcion final de funcion con valor introducido

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,24 +1,12 @@
-var d = document.getElementById("dibujito");
-var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colorcito = "#FAA";
-var texto = document.getElementById("texto");
+
+var texto = document.getElementById("texto_lineas");
 var boton = document.getElementById("botoncito");
 boton.addEventListener("click",dibujoPorClick);
 
+var d = document.getElementById("dibujito");
+var ancho = d.width;
+var lienzo = d.getContext("2d");
 
-for(l=0; l < lineas; l++)
-{
-	yi = 10 * l;
-	xf = 10 * (l+1);
-	dibujarLinea(colorcito,0,yi,xf,300);
-	console.log("Linea" + 1);
-}
-
-dibujarLinea(colorcito, 1,1,1,300);
-dibujarLinea(colorcito, 1,299,299,299);
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
 	lienzo.beginPath();
@@ -33,5 +21,21 @@ function dibujoPorClick()
 
 {
 
-	alert("No me toques ahí");
+	var lineas = parseInt(texto.value);
+	var l = 0;
+	var yi, xf;
+	var colorcito = "#FAA";
+	var espacio = ancho / lineas;
+
+	for(l=0; l < lineas; l++)
+	{
+		yi = espacio * l;
+		xf = espacio * (l+1);
+		dibujarLinea(colorcito,0,yi,xf,300);
+		console.log("Linea" + 1);
+	}
+	dibujarLinea(colorcito, 1,1,1,300);
+	dibujarLinea(colorcito, 1,299,299,299);
+
 }
+
