a5814f02ffe87ad13c03a72085853eed38690cef
Luis Chitala <itastetaco@gmail.com>
2019-07-23 03:40:26 -0500

    Cambio a ciclo for

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -4,13 +4,12 @@ var lineas = 30;
 var l = 0;
 var yi, xf;
 var colorcito = "#FAA";
-while(l < lineas)
+for(l=0; l < lineas; l++)
 {
 	yi = 10 * l;
 	xf = 10 * (l+1);
 	dibujarLinea(colorcito,0,yi,xf,300);
 	console.log("Linea" + 1);
-	l = l + 1;
 }
 
 dibujarLinea(colorcito, 1,1,1,300);
