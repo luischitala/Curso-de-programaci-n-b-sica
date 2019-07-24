f76cbf385d251252c297cc0db2d978f08d604428
Luis Chitala <itastetaco@gmail.com>
2019-07-23 21:20:33 -0500

    Separacion externa de clase y muestra exitosa de arrays dentro de su clase

---
+++ pakiman/paki.js
@@ -0,0 +1,14 @@
+var imagenes = [];
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Pokacho"] = "pollo.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
+
+var Pokacho = new Pakiman("Pokacho",100,30);
+var Cauchin = new Pakiman("Cauchin",80,50);
+var Tocinauro = new Pakiman("Tocinauro",120,40);
+
+
+Pokacho.mostrar();
+Cauchin.mostrar();
+Tocinauro.mostrar();
