b3d9ce433c04ee93a545f5ed5270cc0bf01300e2
Luis Chitala <itastetaco@gmail.com>
2019-07-23 20:11:52 -0500

    Solucion final del ejercicio fizzbuzz

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -3,19 +3,30 @@ var divisible = false;
 for(var i = 1; i<=100; i++)
 {
 	divisible = false;
-	if(i % 3 == 0)
+	if(esDivisible(i,3))
 	{
 		document.write("Fizz");
-		divisible = true;
+
 	}
-	if(i % 5 == 0 )
+	if(esDivisible(i,5))
 	{
 		document.write("buzz");	
-		divisible = true;
+		
 	}
-	if(!divisible)
+	if(!esDivisible(i, 3) && !esDivisible(i%5))
 	{
 		document.write(i);
 	}
 	document.write("<br/>");
+
+
+
+	function esDivisible(num, divisor)
+	{
+		if(num % divisor == 0)
+
+		{
+			return true;
+		}	
+	}
 }
